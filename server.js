a5e0e9daf12d01746e9e57012d6f8d7335325eb9
Rebecca Berto <beccaseawell@rebeccas-air.mynetworksettings.com>
2026-02-22 20:53:12 -0500

    Add dynamic port for deployment

--- server.js
+++ server.js
@@ -169,6 +169,8 @@ better.')">
   }
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
